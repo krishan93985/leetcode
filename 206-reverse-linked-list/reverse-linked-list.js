5e2c808682476fb2ad162d4881dd8c9be24baa0e
Krishan Gopal <52655250+krishan93985@users.noreply.github.com>
2025-05-30 17:32:36 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (42.74%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head || !head.next) return head;
+
+    let prev = null, next = head.next;
+
+    while(next){
+        head.next = prev;
+        prev = head;
+        head = next;
+        next = next.next;
+    }
+
+    if(!next) head.next = prev;
+
+    return head;
+};
