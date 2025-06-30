6455f071d797ea67ef28b5864816f1ab0988143c
Krishan Gopal <52655250+krishan93985@users.noreply.github.com>
2025-06-30 21:38:20 +0530

    Time: 47 ms (24.80%) | Memory: 84.1 MB (13.55%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -3,16 +3,22 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    const map = new Set(nums)
+    const map = new Map()
+
+    for(let num of nums){
+        map.set(num, true)
+    }
 
     let maxCount = 0;
-    for(let num of map){
+    for(num of nums){
         if(!map.has(num-1)){
             let currNum = num, count = 0;
+            if(map.get(currNum) === -1) continue;
             while(map.has(currNum)){
                 count++;
                 currNum++;
             }
+            map.set(num, -1);
             maxCount = maxCount > count ? maxCount:count;
         }
     }
