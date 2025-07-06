7f4774010933fa1f0bdcfb1a564fddbf7dd8786e
Krishan Gopal <52655250+krishan93985@users.noreply.github.com>
2025-07-06 17:38:54 +0530

    Time: 98 ms (5.09%) | Memory: 64.3 MB (5.06%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    const maxLeft = [], maxRight = []
+
+    maxLeft.push(0)
+    for(let i=1; i<height.length; i++){
+        maxLeft.push(Math.max(maxLeft[maxLeft.length - 1], height[i-1]))
+    }
+
+    maxRight.push(0)
+    for(let i=height.length-2; i>=0; i--){
+        maxRight.unshift(Math.max(maxRight[0],height[i+1]))
+    }
+
+    let water = 0;
+    for(let [key, h] of height.entries()){
+        let currWater = Math.min(maxLeft[key], maxRight[key]) - h
+        water += currWater > 0 ? currWater : 0
+    }
+
+    return water;
+};
