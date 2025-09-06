973e3eeca34411e9ebe916e1a897aa126221fa65
Krishan Gopal <52655250+krishan93985@users.noreply.github.com>
2025-09-06 19:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 60.2 MB (9.30%) - LeetSync

--- 90-subsets-ii/subsets-ii.js
+++ 90-subsets-ii/subsets-ii.js
@@ -3,23 +3,23 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-    const output = [];
-    const arr = [];
-    nums.sort((a,b) => a-b);
+  const subsets = []
+  
+  nums.sort((a,b) => a-b)
 
-    generateSubsets(nums, 0, arr, output)
+  const generateSubsets = (index, arr = []) => {
+    subsets.push([...arr])
 
-    return output;
-};
-
-var generateSubsets = function(nums, idx, arr, output) {
-    output.push([...arr])
-
-    for(let i=idx; i<nums.length; i++){
-        if(i !== idx && nums[i] === nums[i-1]) continue;
+    for(let i=index; i<nums.length; i++){
+        if(i!==index && nums[i] === nums[i-1]) continue;
 
         arr.push(nums[i]);
-        generateSubsets(nums, i+1, arr, output);
-        arr.pop(nums[i]);
+        generateSubsets(i+1, arr);
+        arr.pop();
     }
-}
+  }
+
+  generateSubsets(0, [])
+
+  return subsets;
+};
