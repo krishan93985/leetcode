04caa3a287598277a043933570725caff97a3cf1
Krishan Gopal <52655250+krishan93985@users.noreply.github.com>
2025-09-07 21:32:15 +0530

    Time: 11 ms (38.52%) | Memory: 57.7 MB (53.53%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.js
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -4,30 +4,46 @@
  */
 var orangesRotting = function(grid) {
     let minute = 0;
-    const queue = [], visited = Array.from({ length: grid.length }, 
-    () => new Array(grid[0].length).fill(0));
+    const queue = [], visited = [], n = grid.length, m = grid[0].length;
 
-    for(let i=0; i<grid.length; i++){
-        for(let j=0; j<grid[i].length; j++){
+    for(let i=0; i<n; i++){
+        visited.push([])
+        for(let j=0; j<m; j++){
             if(grid[i][j] === 2){
                 queue.push({
                     orange:[i,j],
                     time:0
                 })
                 visited[i][j] = 2
+            } else {
+                visited[i][j] = 0;
             }
         }
     }
 
+    const drow = [-1,0,1,0];
+    const dcol = [0,1,0,-1]
+
     while(queue.length){
-        let {orange, time} = queue.shift();
+        let {orange:[row,col], time} = queue.shift();
 
         minute = Math.max(minute, time);
-        markRotten(grid, orange, queue, visited, time);
+        for(let i=0; i<4; i++){
+            let nrow = row + drow[i];
+            let ncol = col + dcol[i];
+
+            if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && visited[nrow][ncol] !== 2 && grid[nrow][ncol] === 1){
+                queue.push({
+                    orange:[nrow,ncol],
+                    time:time+1
+                })
+                visited[nrow][ncol] = 2;
+            }
+        }
     }
 
-    for(let i=0; i<grid.length; i++){
-        for(let j=0; j<grid[i].length; j++){
+    for(let i=0; i<n; i++){
+        for(let j=0; j<m; j++){
             if(grid[i][j] === 1 && visited[i][j] !== 2){
                 return -1;
             }
