5eddeb9a11eb96288a06fc8012d231d5907aa9b6
Krishan Gopal <52655250+krishan93985@users.noreply.github.com>
2025-12-28 14:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 57 MB (92.64%) - LeetSync

--- 733-flood-fill/flood-fill.js
+++ 733-flood-fill/flood-fill.js
@@ -7,16 +7,14 @@
  */
 var floodFill = function(image, sr, sc, color) {
     const rows = image.length, cols = image[0].length;
-    const visited = Array.from({ length:rows }, () => new Array(cols).fill(false))
     const firstColor = image[sr][sc];
     const dir = [[0,1],[0,-1],[1,0],[-1,0]]
 
     if(color === firstColor) return image;
 
     function sweep(i, j){
-        if(i < 0 || j < 0 || i === rows || j === cols || visited[i][j] || image[i][j] !== firstColor) return;
-
-        visited[i][j] = true;
+        if(i < 0 || j < 0 || i === rows || j === cols || image[i][j] !== firstColor) return;
+        
         image[i][j] = color;
 
         for(let k=0; k<4; k++){
